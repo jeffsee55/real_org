ca4d7e8ae330857ec4faf1224444f1048ed1e9d6
Jeff <jeffsee.55@gmail.com>
2015-02-10 14:07:21 -0500

    Added Dotenv.load to application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv.load
+
 module RealOrg
   class Application < Rails::Application
     config.i18n.enforce_available_locales = true
